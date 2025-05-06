8fb7b885ea2b744bd170a84c32dc47c2886041a0
saifulshihab <shihabmd1970@gmail.com>
2025-05-07 00:27:17 +0600

    fix eslint & setup prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,23 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __dirname
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
+      "no-console": "warn"
+    }
+  })
 ];
 
 export default eslintConfig;
